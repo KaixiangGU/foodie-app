b47b78fdbb3b8f6bf6fbe481fd7c9fe5e0c2fb98
Kevin GU <vanrousgkx@hotmail.com>
2022-07-22 17:14:04 +0800

    fixed api url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://foodie-app-project.herokuapp.com" });
 
 // const url = "https://foodie-app-project.herokuapp.com/recipes";
 // const url = "http://localhost:5000/recipes";
--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,13 @@ import App from "./App";
 import { StateContext } from "./context/StateContext";
 import { Provider } from "react-redux";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-
 import store from "./store/store";
-import { Google } from "@mui/icons-material";
+
+const CLIENT_ID = "1048352977453-i02u1k8gqqkg44mlbrq4dnghounhm42p.apps.googleusercontent.com";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <GoogleOAuthProvider clientId="1048352977453-i02u1k8gqqkg44mlbrq4dnghounhm42p.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId={CLIENT_ID}>
     <StateContext>
       <Provider store={store}>
         <App />
